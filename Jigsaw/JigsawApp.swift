32b2ca9faededba76076fe07538282b99ba341a1
ayemereodia2 <odiadavid2@gmail.com>
2024-04-19 19:32:12 -0300

    Initial Commit

---
+++ Jigsaw/JigsawApp.swift
@@ -0,0 +1,17 @@
+//
+//  JigsawApp.swift
+//  Jigsaw
+//
+//  Created by ANDELA on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct JigsawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
